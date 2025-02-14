5ebac29914c37752b03bf1cdd28ce7e64537dc82
sawruins <2911499062@qq.com>
2025-02-14 15:49:23 +0800

    deepseek R-1  C#环境下的简单实现

--- SWS.Core/SWS.Core/Program.cs
+++ SWS.Core/SWS.Core/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Shaoye!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Shaoye!");
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("给DeepSeek发送消息:");
+        while (true)
+        {
+            string input = Console.ReadLine();
+            int randomWaiting = new Random().Next(10000, 40000);
+            Console.WriteLine("\n思考中...");
+            Thread.Sleep(randomWaiting);
+            Console.WriteLine("服务器繁忙，请稍后再试。");
+        }
+    }
+}
+
